b4d6a975b45162a044e71764c57f8a2c7121209f
unknown <brosjayme@gmail.com>
2023-04-30 20:01:01 +0100

    firebase auth setup

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
   return (
     <>
     <div className='p-4 box'>
-        <h2 className='mb-3'>Form Auth Signup</h2>
+        <h2 className='mb-3'>Form Auth Login</h2>
         <Form>
             <Form.Group className='mb-3' controlId='formBasicEmail'>
               <Form.Control type='email' placeholder='Email address'></Form.Control>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,51 +1,60 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Alert, Form } from 'react-bootstrap';
+import { Form, Alert } from 'react-bootstrap';
 import { Button } from 'react-bootstrap'
 import { useUserAuth } from '../context/UserAuthContext';
 
+
 const Signup = () => {
-    const [email, setEmail] =useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const { Signup } = useUserAuth();
-    const handlesubmit = async (e) => {
-        e.preventDefault();
-        setError('')
-        try {
-            await Signup(email,password, error);
-        }catch (err) {
-       setError(err.message);
-        }
-    } 
-  return (
-    <>
-    <div className='p-4 box'>
-        <h2 className='mb-3'>Form Auth Signup</h2>
-        {error && <Alert variant='danger'>{error}</Alert>}
-        <Form onSubmit= {handlesubmit}>
-            <Form.Group className='mb-3' controlId='formBasicEmail'>
-              <Form.Control type='email' placeholder='Email address'
-             onChange ={ (e) => setEmail(e.target.value)}/>
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
+    const [password, setPassword] = useState("");
+    const { signUp } = useUserAuth();
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      setError("");
+      try {
+        await signUp(email, password);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+  
+    return (
+      <>
+        <div className="p-4 box">
+          <h2 className="mb-3">Firebase Auth Signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Email address"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formBasicPassword'>
-            <Form.Control type='password' placeholder='Password'
-              onChange ={ (e) => setPassword(e.target.value)}/>
-          </Form.Group>
-
-          <div className='d-grid gap-2'>
-            <Button variant='primary' type='submit'>
-                sign up
-            </Button>
-          </div>
-        </Form>
-    </div>
-    <div className='p-4 box mt-3 text-center'>
-        Already have an account?  <Link to='/'>Log In</Link>
-    </div>
-    </>
-  )
-}
-
-export default Signup
+  
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+  
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="Submit">
+                Sign up
+              </Button>
+            </div>
+          </Form>
+        </div>
+        <div className="p-4 box mt-3 text-center">
+          Already have an account? <Link to="/">Log In</Link>
+        </div>
+      </>
+    );
+  };
+  
+  export default Signup;
